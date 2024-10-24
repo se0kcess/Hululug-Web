7274a7ebbc0f58841b0b377a0de8deca88a6572d
MinSeok Kim <kimminsuk051@gmail.com>
2024-10-24 15:20:30 +0900

    initial commit

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,47 @@
+import { Global, css } from '@emotion/react';
+
+const globalStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+      'Open Sans', 'Helvetica Neue', sans-serif;
+    background-color: #fff;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button,
+  input,
+  select,
+  textarea {
+    background-color: transparent;
+    border: 0;
+    &:focus {
+      outline: none;
+      box-shadow: none;
+    }
+  }
+
+  a,
+  button {
+    cursor: pointer;
+  }
+
+  ul,
+  ol {
+    list-style: none;
+  }
+`;
+
+export function GlobalStyles() {
+  return <Global styles={globalStyles} />;
+}
