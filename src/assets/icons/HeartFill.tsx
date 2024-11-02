fd8479ec3c226ef5182da76b5e0f205e4f182688
MinSeok Kim <kimminsuk051@gmail.com>
2024-11-02 19:52:08 +0900

    feat: MyRecipeCard 컴포넌트 구현

--- src/assets/icons/HeartFill.tsx
+++ src/assets/icons/HeartFill.tsx
@@ -6,8 +6,8 @@ interface HeartIconProps extends React.SVGProps<SVGSVGElement> {
 
 export const HeartFill = ({ isliked, ...props }: HeartIconProps) => (
   <svg
-    width="24"
-    height="24"
+    width="18"
+    height="18"
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
