17a1cfb3f16d58fdca211e2c78b11da5d0f93404
ysmuei <108169494+ysmuei@users.noreply.github.com>
2024-11-05 16:46:04 +0900

    feat:#48-ramenRecipe 타입 파일 생성

---
+++ src/types/ramenRecipe.ts
@@ -0,0 +1,18 @@
+export type RamenType = {
+  id: number;
+  name: string;
+};
+
+export interface RamenRecipe {
+  _id: string;
+  recipe_id: string;
+  title: string;
+  thumbnail: string;
+  tags: string[];
+  likes: number;
+  created_at: string;
+  writer: {
+    nickname: string;
+    profile_image: string;
+  };
+}
