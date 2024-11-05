e40e1e22fe78257873f10d67d0dc63597ad7bfcf
ysmuei <108169494+ysmuei@users.noreply.github.com>
2024-11-05 14:09:22 +0900

    feat:#46-RamenTypeSelect 수정

--- src/components/PostPage/RamenTypeSelect/RamenTypeSelect.stories.tsx
+++ src/components/PostPage/RamenTypeSelect/RamenTypeSelect.stories.tsx
@@ -1,38 +1,43 @@
-import { useState } from 'react';
+import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
-import RamenTypeSelect from './RamenTypeSelect';
+import RamenTypeSelect from '@/components/PostPage/RamenTypeSelect/RamenTypeSelect';
+import useRecipeStore from '@/store/recipeStore';
 
-export default {
-  title: 'Components/RamenTypeSelect',
-  component: RamenTypeSelect,
-  argTypes: {
-    onSelect: { action: 'selected' },
-    onDisSelect: { action: 'deselected' },
-  },
-} as Meta<typeof RamenTypeSelect>;
+// Zustand 상태를 초기화하는 Provider
+const StoreProvider = ({ children }: { children: React.ReactNode }) => {
+  const { setSelectedRamen, setRamenSelected } = useRecipeStore();
 
-const Template: StoryFn = (args) => {
-  const [selected, setSelected] = useState(false);
+  React.useEffect(() => {
+    setSelectedRamen([]); // 선택된 라면 초기화
+    setRamenSelected(false); // 초기 상태 false 설정
+  }, [setSelectedRamen, setRamenSelected]);
 
-  const handleSelect = () => {
-    setSelected(true);
-    args.onSelect();
-  };
-
-  const handleDeselect = () => {
-    setSelected(false);
-    args.onDisSelect();
-  };
-
-  return (
-    <div>
-      <RamenTypeSelect onSelect={handleSelect} onDisSelect={handleDeselect} />
-      <div style={{ marginTop: '20px', fontWeight: 'bold' }}>
-        {selected ? '라면 종류가 선택되었습니다.' : '라면 종류가 선택되지 않았습니다.'}
-      </div>
-    </div>
-  );
+  return <>{children}</>;
 };
 
+export default {
+  title: 'Components/PostPage/RamenTypeSelect',
+  component: RamenTypeSelect,
+  decorators: [(storyFn) => <StoreProvider>{storyFn()}</StoreProvider>],
+} as Meta;
+
+const Template: StoryFn = () => <RamenTypeSelect />;
+
 export const Default = Template.bind({});
-Default.args = {};
+Default.storyName = 'Default View';
+
+export const WithSelectedRamen = Template.bind({});
+WithSelectedRamen.storyName = 'With Selected Ramen';
+WithSelectedRamen.decorators = [
+  (storyFn) => {
+    const { setSelectedRamen, setRamenSelected } = useRecipeStore();
+    React.useEffect(() => {
+      setSelectedRamen([
+        { id: 1, name: 'Shoyu Ramen' },
+        { id: 2, name: 'Miso Ramen' },
+      ]); // 예시 라면 설정
+      setRamenSelected(true);
+    }, [setSelectedRamen, setRamenSelected]);
+    return storyFn();
+  },
+];
