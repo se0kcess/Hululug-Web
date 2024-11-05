3d95651213f097314e6a2e53ab69c5537108051f
ysmuei <108169494+ysmuei@users.noreply.github.com>
2024-11-05 16:44:28 +0900

    feat:#48-searchStore zustand store 생성

---
+++ src/store/searchStore.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+
+interface SearchState {
+  query: string;
+  setQuery: (query: string) => void;
+}
+
+const useSearchStore = create<SearchState>((set) => ({
+  query: '',
+  setQuery: (query) => set({ query }),
+}));
+
+export default useSearchStore;
